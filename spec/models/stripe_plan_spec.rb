0a65f89052a8375448e1b6d150ac719fc1c95404
Patrick O'Doherty <p@trickod.com>
2015-01-22 21:42:15 -0800

    Install Stripe w/ Gemfile. Finish StripePlan spec

--- spec/models/stripe_plan_spec.rb
+++ spec/models/stripe_plan_spec.rb
@@ -15,6 +15,19 @@ describe StripePlan, type: :model do
     expect(plan.stripe_id).to_not be_blank
   end
 
-  it 'creates a plan in Stripe on create'
+  it 'creates a plan in Stripe on create' do
+    expect(Stripe::Plan).to receive(:create).once.with(
+      id: "noisebridge_donation_10",
+      name: "$10 / month",
+      amount: 10_00,
+      currency: 'usd',
+      interval: 'month',
+      statement_descriptor: "Noisebridge donation"
+    )
+    StripePlan.create(
+      amount: 10_00,
+      name: "$10/month"
+    )
+  end
 end
 
