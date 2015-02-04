7fccfa914e1c427ea46288e7eb72102e4228ab5a
Patrick O'Doherty <p@trickod.com>
2015-02-03 22:46:14 -0800

    Make the donors email field required

---
+++ db/migrate/20150204064444_make_donor_email_required.rb
@@ -0,0 +1,9 @@
+class MakeDonorEmailRequired < ActiveRecord::Migration
+  def up
+    change_column(:donors, :email, :string, null: false)
+  end
+
+  def down
+    change_column(:donors, :email, :string, null: true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127072345) do
+ActiveRecord::Schema.define(version: 20150204064444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "donors", force: true do |t|
-    t.string   "email"
+    t.string   "email",              null: false
     t.string   "stripe_customer_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
