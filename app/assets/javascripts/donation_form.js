4e6c861c86aa93a31a68017900d6ac13b8e9bed8
Patrick O'Doherty <p@trickod.com>
2015-02-21 15:48:30 -0800

    half-done integration w/ checkout.js

--- app/assets/javascripts/donation_form.js
+++ app/assets/javascripts/donation_form.js
@@ -28,31 +28,5 @@ jQuery(function($) {
       custom_input.remove();
     }
   });
-
-  $('form.donation-form').submit(function(event) {
-    var $form = $(this);
-
-    resetErrors($form);
-
-    $form.find('button').prop('disabled', true);
-
-    Stripe.card.createToken({
-      number: $form.find(".cc-number").val(),
-      cvc: $form.find(".cc-cvc").val(),
-      exp_month: $form.find(".cc-exp").payment('cardExpiryVal').month,
-      exp_year: $form.find(".cc-exp").payment('cardExpiryVal').year
-    }, function(status, response) {
-      if (response.error) {
-        displayError($form, response.error);
-        $form.find('button').prop('disabled', false);
-      } else {
-        var token = response.id;
-        $form.append($('<input type="hidden" name="donor[stripe_token]" />').val(token));
-        $form.get(0).submit();
-      }
-    });
-
-    return false;
-  });
 });
 
