5198bcdc7d2068fb5790dbaff9e4b065ecfa4182
Patrick O'Doherty <p@trickod.com>
2015-02-21 16:07:17 -0800

    Add "dues" flag to Subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209040539) do
+ActiveRecord::Schema.define(version: 20150222000634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,10 @@ ActiveRecord::Schema.define(version: 20150209040539) do
     t.datetime "cancelled_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "donor_id",                  null: false
-    t.integer  "plan_id",                   null: false
-    t.string   "stripe_subscription_id",    null: false
+    t.integer  "donor_id",                                  null: false
+    t.integer  "plan_id",                                   null: false
+    t.string   "stripe_subscription_id",                    null: false
+    t.boolean  "dues",                      default: false, null: false
   end
 
 end
