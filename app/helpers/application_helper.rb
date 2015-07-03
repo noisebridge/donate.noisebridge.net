e641537c82018889f8a650d4ef87293a2b134e4b
Patrick O'Doherty <p@trickod.com>
2015-07-02 22:54:41 -0700

    Add footer w/ license, link to source & current version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def current_version
+    if Rails.env.production?
+      # TODO: Replace this with a suitable environment variable
+      Dir.pwd.match(/noisebridge-donate-([0-9a-f]{40})/)[1]
+    else
+      "development"
+    end
+  end
 end
