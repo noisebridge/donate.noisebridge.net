c9ca3ef49c081fbb314cb2b4863b1f1e1254256b
Patrick O'Doherty <p@trickod.com>
2016-03-12 18:08:38 -0800

    Render project page templates where appropriate
    Allow the cretation of project specific partials in the projects/ views
    directory which will be rendered alongside the donation form when the
    project page is viewed.
    
    The first page to have such a template will be the laser page.
    
    Add SuggestedDonationAmount class which is used to populate the amounts
    drop-down based on the current project being rendered.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < DonationsController
   def show
     @name = params[:id]
+    @should_render_partial = template_exists?(@name, _prefixes, true)
   end
 
   def index
---
+++ app/models/suggested_donation_amount.rb
@@ -0,0 +1,24 @@
+class SuggestedDonationAmount
+
+  SUGGESTED_AMOUNTS = {
+    laser: [
+      20,
+      50,
+      100,
+      500,
+      1000
+    ],
+    default: [
+      10,
+      20,
+      40,
+      80,
+      160
+    ]
+  }
+
+  def self.for_project(name)
+    SUGGESTED_AMOUNTS[name.to_sym] ||
+      SUGGESTED_AMOUNTS[:default] 
+  end
+end
