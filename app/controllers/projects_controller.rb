f5b4ebc310ccfa4e3efdc14f80a7fbeb32e8ce3a
Patrick O'Doherty <p@trickod.com>
2016-03-17 21:42:16 -0700

    Revert "Revert "Merge pull request #60 from noisebridge/patrickod/project-progress-bar""
    This reverts commit f4f36d2cde55006307f6abffb08dc201f0854759.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < DonationsController
   def show
     @name = params[:id]
+    @project = Project.find(params[:id].to_sym)
     @should_render_partial = template_exists?(@name, _prefixes, true)
   end
 
