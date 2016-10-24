9d2c0573bb7ca058d1adaa9b7e20242c54ceb3d9
Patrick O'Doherty <p@trickod.com>
2016-10-24 12:25:51 +0100

    Clean up project & suggested donation amount code
    Remove suggested_donation_amounts from Project class entirely to prevent
    confusion. Add in the suggested donation amounts for stickers.
    
    Remove unused percentage_raised method from Project.

--- app/lib/project.rb
+++ app/lib/project.rb
@@ -1,37 +1,14 @@
 class Project
   PROJECTS = {
-    stickers: {
-      suggested_donation_amounts: [
-        5,
-        10,
-        25,
-        50,
-        100,
-      ],
-    },
     laser: {
-      suggested_donation_amounts: [
-        1000,
-        500,
-        100,
-        50,
-        20,
-      ],
       # All amounts in dollar cents
       fundraising_goal: 6_000_00
     },
     default: {
-      suggested_donation_amounts: [
-        10,
-        20,
-        40,
-        80,
-        160
-      ],
     }
   }
 
-  attr_accessor :name, :fundraising_goal, :suggested_donation_amounts
+  attr_accessor :name, :fundraising_goal
 
   def self.find(name)
     PROJECTS.map do |project, params|
@@ -44,16 +21,11 @@ class Project
 
   def initialize(name, params)
     @name = name
-    @suggested_donation_amounts = params[:suggested_donation_amounts]
     @fundraising_goal = params[:fundraising_goal]
   end
 
   def amount_raised
     @amount_raised = Charge.tagged.where(tag: @name).sum(:amount)
-  end
-
-  def percentage_raised
-    amount_raised / (fundraising_goal.to_f) * 100
   end
 
   def has_fundraising_goal?
