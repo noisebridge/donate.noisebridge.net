e18eaac54cfb06360e3062a14cf3f21a47a4ed99
Patrick O'Doherty <p@trickod.com>
2016-10-24 12:33:23 +0100

    clean up numerous deprecations and warnings
    Fix child_src frame_src deprecation warning for secureheaders.
    Fix rspec warning about stub on new w/ and_call_original
    Fix deprecation notice for skip_before_filter

--- app/controllers/api/stripe_events_controller.rb
+++ app/controllers/api/stripe_events_controller.rb
@@ -1,5 +1,5 @@
 class Api::StripeEventsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def create
     # Parse incoming webhook. Retrieve the specified event from Stripe to
